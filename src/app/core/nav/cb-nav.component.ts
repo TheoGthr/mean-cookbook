f4c6126266ba9e6bbdaf2fe0f1e84e03ee0aaaed
TheoGthr <theogauthier13@gmail.com>
2019-11-22 10:35:44 -0500

    mat elevation nav

--- src/app/core/nav/cb-nav.component.ts
+++ src/app/core/nav/cb-nav.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'cb-nav',
-  styleUrls: ['./cb-nav.component.scss'],
+  selector: "cb-nav",
+  styleUrls: ["./cb-nav.component.scss"],
   template: `
     <mat-sidenav-container fullscreen class="app-sidenav">
       <mat-sidenav-content class="sidenav-content">
-        <mat-toolbar color='primary'>
+        <mat-toolbar color="primary" class="mat-elevation-z4">
           <a routerLink="/" class="logo">My Cookbook</a>
         </mat-toolbar>
         <div id="routes">
